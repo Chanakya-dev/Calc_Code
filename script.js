082e4648a574bd0785675a676a7a0b5230e0cbac
Chanakya-dev <Chanakyamanas123@gmail.com>
2025-01-09 13:37:05 +0530

    Alligned Buttons

--- script.js
+++ script.js
@@ -1,55 +1,93 @@
-const display = document.getElementById('display');
-const buttons = document.querySelectorAll('.buttons button');
+class Calculator {
+    constructor() {
+        this.display = document.getElementById('display');
+        this.buttons = document.querySelectorAll('.buttons button');
+        this.currentNumber = '';
+        this.previousNumber = '';
+        this.operation = '';
+        this.init();
+    }
 
-let currentNumber = '';
-let previousNumber = '';
-let operation = '';
+    init() {
+        this.buttons.forEach(button => {
+            button.addEventListener('click', () => {
+                this.handleClick(button.textContent);
+            });
+        });
+    }
 
-buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        const value = button.textContent;
-
-        if (value === 'C') {
-            currentNumber = '';
-            previousNumber = '';
-            operation = '';
-            display.value = '';
-        } else if (value === '<') {
-            currentNumber = currentNumber.slice(0, -1);
-            display.value = currentNumber;
-        } else if (value === '=') {
-            if (previousNumber !== '' && operation !== '') {
-                const result = calculate(previousNumber, currentNumber, operation);
-                display.value = result;
-                previousNumber = result;
-                currentNumber = '';
-                operation = '';
-            }
-        } else if (['+', '-', '*', '/'].includes(value)) {
-            operation = value;
-            previousNumber = currentNumber;
-            currentNumber = '';
-        } else {
-            currentNumber += value;
-            display.value = currentNumber;
+    handleClick(value) {
+        switch (value) {
+            case 'C':
+                this.clear();
+                break;
+            case '<':
+                this.backspace();
+                break;
+            case '=':
+                this.calculate();
+                break;
+            case '+':
+            case '-':
+            case '*':
+            case '/':
+                this.setOperation(value);
+                break;
+            default:
+                this.appendNumber(value);
         }
-    });
-});
+    }
 
-function calculate(num1, num2, operation) {
-    num1 = parseFloat(num1);
-    num2 = parseFloat(num2);
+    clear() {
+        this.currentNumber = '';
+        this.previousNumber = '';
+        this.operation = '';
+        this.display.value = '';
+    }
 
-    switch (operation) {
-        case '+':
-            return num1 + num2;
-        case '-':
-            return num1 - num2;
-        case '*':
-            return num1 * num2;
-        case '/':
-            return num1 / num2;
-        default:
-            return 0;
+    backspace() {
+        this.currentNumber = this.currentNumber.slice(0, -1);
+        this.display.value = this.currentNumber;
+    }
+
+    calculate() {
+        if (this.previousNumber !== '' && this.operation !== '') {
+            const result = this.calculateResult();
+            this.display.value = result;
+            this.previousNumber = result;
+            this.currentNumber = '';
+            this.operation = '';
+        }
+    }
+
+    setOperation(value) {
+        this.operation = value;
+        this.previousNumber = this.currentNumber;
+        this.currentNumber = '';
+    }
+
+    appendNumber(value) {
+        this.currentNumber += value;
+        this.display.value = this.currentNumber;
+    }
+
+    calculateResult() {
+        const num1 = parseFloat(this.previousNumber);
+        const num2 = parseFloat(this.currentNumber);
+
+        switch (this.operation) {
+            case '+':
+                return num1 + num2;
+            case '-':
+                return num1 - num2;
+            case '*':
+                return num1 * num2;
+            case '/':
+                return num1 / num2;
+            default:
+                return 0;
+        }
     }
 }
+
+new Calculator();
